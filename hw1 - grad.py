9f69f66e7ef17099880d1411ff2339fd2da85299
ryan acree <eyobtewelde33@gmail.com>
2024-09-16 04:06:10 -0500

    Added comments to the code.

--- hw1 - grad.py
+++ hw1 - grad.py
@@ -5,9 +5,9 @@
 import numpy as np
 h_bar, m, L = 1, 1, 1
 
-
 grid = np.linspace(-L / 2, L / 2, 2000, endpoint=True)
 delt_x = grid[1] - grid[0]
+
 
 #construct the laplacian matrix
 
@@ -16,29 +16,43 @@ off_diagonal = np.eye(2000, 2000, 1) + np.eye(2000, 2000, -1)
 
 lap_matrix = (1 / (delt_x ** 2)) * (-2 * iden_matrix + off_diagonal)
 
+
 #construct the hamiltonian matrix
 
 ham_matrix = (-1 / 2) * lap_matrix
 
+
 #solve for eigenvalues and eigenfunctions
 
 eigenvalue, eigenvector = np.linalg.eig(ham_matrix)
+
+
+#sort the eigenvalues in increasing order
 sorted_eigenval = np.sort(eigenvalue)
 
+
+#extract the first seven eignevalues from the ordered array
 first_seven_eigvals = (sorted_eigenval[:7])
 
 
+#extract the eigenvectors that correspond to the first seven eigenvalues
 first_seven_eigvecs = []
 for val in first_seven_eigvals:
     index = np.where(eigenvalue == val)
     first_seven_eigvecs.append(eigenvector[:,index])
 
+
+#reduce unnecessary dimensionality in the eigenvector array so that it can plotted
 squeezed_eigvecs = np.squeeze(first_seven_eigvecs[:5])
 
 import matplotlib.pyplot as plt
 
+
+#iterate over the wavefunctions and plot them against the position in the potential well
 for i in range(squeezed_eigvecs.shape[0]):
     plt.plot(grid, squeezed_eigvecs[i], label=str(i))
+
+
 
 plt.title("Wavefunctions in a 1-D potential well")
 plt.xlabel("L (a0)")
