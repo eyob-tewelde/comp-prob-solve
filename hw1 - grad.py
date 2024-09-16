be5bec730ded2956491bdea795b6adb682c50dd2
ryan acree <eyobtewelde33@gmail.com>
2024-09-16 03:57:41 -0500

    Bulk of the work done for the graduate supplement. Need to add comments and tidy up code.

---
+++ hw1 - grad.py
@@ -0,0 +1,52 @@
+#hw1 - grad
+
+#define constants
+
+import numpy as np
+h_bar = 1
+m = 1
+L = 1
+
+grid = np.linspace(-L / 2, L / 2, 2000, endpoint=True)
+delt_x = grid[1] - grid[0]
+
+#construct the laplacian matrix
+
+iden_matrix = np.identity(2000)
+off_diagonal = np.eye(2000, 2000, 1) + np.eye(2000, 2000, -1)
+
+lap_matrix = (1 / (delt_x ** 2)) * (-2 * iden_matrix + off_diagonal)
+
+#construct the hamiltonian matrix
+
+ham_matrix = (-1 / 2) * lap_matrix
+
+#solve for eigenvalues and eigenfunctions
+
+eigenvalue, eigenvector = np.linalg.eig(ham_matrix)
+sorted_eigenval = np.sort(eigenvalue)
+
+first_seven_eigvals = (sorted_eigenval[:7])
+
+
+first_seven_eigvecs = []
+for val in first_seven_eigvals:
+    index = np.where(eigenvalue == val)
+    first_seven_eigvecs.append(eigenvector[:,index])
+
+squeezed_eigvecs = np.squeeze(first_seven_eigvecs[:5])
+
+import matplotlib.pyplot as plt
+
+for i in range(squeezed_eigvecs.shape[0]):
+    plt.plot(grid, squeezed_eigvecs[i], label=str(i))
+
+plt.title("Wavefunctions in a 1-D potential well")
+plt.xlabel("L (a0)")
+plt.ylabel("Wavefunction")
+plt.legend(fontsize='small', title="Energy Levels", title_fontsize='medium', bbox_to_anchor=(1.05, 1))
+plt.grid()
+plt.show()
+
+
+
