411339ed377f5d4e0118c5d3778d3f8bfca20c84
Eyob Tewelde <eyob@sixer.dhcp.wustl.edu>
2024-11-13 21:42:11 -0600

    python file and plotting

--- project-1/grand_canonical.py
+++ project-1/grand_canonical.py
@@ -1,4 +1,4 @@
-#GCMC simulation
+# Grand canonical monte carlo simulations of competitive adsorption
 
 
 import numpy as np
@@ -8,9 +8,17 @@ import matplotlib.pyplot as plt
 #Define a function that initializes an empty lattice
 def initialize_lattice(size):
     """
-    
-    
+    Initialzies an empty lattice of dimensions size x size
+
+    Parameters:
+        size: int
+            Dimension of the lattice
+
+    Returns:
+        ndarray:
+            An empty array of dimensions size x size
     """
+
     lattice = np.zeros([size, size])
     return lattice
 
@@ -18,14 +26,22 @@ def initialize_lattice(size):
 #Compute neighbor indices with periodic boundary conditions
 def compute_neighbor_indices(size):
     """
-    
-    
+    Computes the coordinates of the neighboring sites for all sites in an array with dimensions size x size
+
+    Parameters:
+        size: int
+            Dimension of the lattice
+
+    Returns:
+        dict{(int, int): list[(int, int)]}:
+            A dictionary where the key is the site of interest and the value is a list containing the coordinates of all neighboring indices
     """
+
     neighbor_indices = {}
     for x in range(size):
         for y in range(size):
             neighbors = [
-                ((x - 1) % size, y),
+                ((x - 1) % size, y),    #Compute neighboring indices
                 ((x + 1) % size, y),
                 (x, (y - 1) % size),
                 (x, (y + 1) % size)
@@ -37,11 +53,31 @@ def compute_neighbor_indices(size):
 #Calculate interaction energy
 def calculate_interaction_energy(lattice, site, particle, neighbor_indices, epsilon_AA, epsilon_BB, epsilon_AB):
     """
-    
-    
+    Calculates the interaction energy of a particle with neighboring particles
+
+    Parameters:
+        lattice: ndarray
+            Represents the simulated lattice. Contains particle A, B or an empty site at all positions within the array
+        site: ndarray
+            Contains the coordinates of the particle of interest
+        particle: int
+            Particle of interest
+        neighbor_indices: dict{(int, int): list[(int, int)]}
+            Contains the coordinates of all neighboring sites
+        epsilon_AA: float
+            Interaction energy between particle A and A
+        epsilon_BB: float
+            Interaction energy between particle B and B
+        epsilon_AB: float
+            Interaction energy between particle A and B
+
+    Returns:
+        float:
+            The total interaction energy felt by the particle of interest
     """
-    x, y = site
-    interaction_energy = 0
+
+    x, y = site # Retrieve site coordinates
+    interaction_energy = 0  # Initialize interaction energy
     for neighbor in neighbor_indices[(x, y)]:
         neighbor_particle = lattice[neighbor]
         if neighbor_particle != 0:
@@ -59,12 +95,33 @@ def calculate_interaction_energy(lattice, site, particle, neighbor_indices, epsi
 
 
 #Attempt to add or remove a particle
-def attempt_move(lattice, N_A, N_B, N_empty, neighbor_indices, params, count):
+def attempt_move(lattice, N_A, N_B, N_empty, neighbor_indices, params):
     """
-    
-    
-    
+    Accepts or rejects a move in the simulation. The possible moves are the adsorption or desorption of particle A or B.
+
+    Parameters:
+        lattice: ndarray
+            Represents the simulated lattice. Contains particle A, B or an empty site at all positions within the array
+        N_A: int
+            Number of particle A's adsorbed to the lattice
+        N_A: int
+            Number of particle B's adsorbed to the lattice
+        N_empty: int
+            Number of empty sites
+        neighbor_indices: dict{(int, int): list[(int, int)]}
+            Contains the coordinates of all neighboring sites
+        params: dict{str: float}
+            Contains the values for the interaction energies, adsorption energies, and temperature
+
+    Returns:
+        N_A: int
+            Number of particle A's adsorbed to the lattice
+        N_B: int
+            Number of particle B's adsorbed to the lattice
+        N_empty: int
+            Number of empty sites
     """
+
     size = np.shape(lattice)[0] #Extract lattice size
     N_sites = size * size   #Calculate number of sites
     beta  = 1 / params['T'] #Extract parameter values
@@ -77,17 +134,17 @@ def attempt_move(lattice, N_A, N_B, N_empty, neighbor_indices, params, count):
     mu_B = params['mu_B']
 
 
-    # DECIDE whether to add or remove a particle (50% chance each)
-    if np.random.rand() < 0.5: #Adding a particle
+    # Decide whether to add or remove a particle (50% chance each)
+    if np.random.rand() < 0.5: # Adding a particle
         if N_empty == 0:
             return N_A, N_B, N_empty  # No empty sites available
         
-        #Select a random empty site
+        # Select a random empty site
         empty_site = np.argwhere(lattice == 0)
         rand_idx = np.random.randint(len(empty_site))
         random_empty_site = empty_site[rand_idx]
 
-        if np.random.rand() < 0.5:  #Adding particle A
+        if np.random.rand() < 0.5:  # Adding particle A
             particle = 1
             mu = mu_A
             epsilon = epsilon_A
@@ -98,20 +155,21 @@ def attempt_move(lattice, N_A, N_B, N_empty, neighbor_indices, params, count):
             epsilon = epsilon_B
             N_s = N_B
     
-        #Calculate change in energy for adding particle
+        #C alculate change in energy for adding particle
         delta_E = epsilon + calculate_interaction_energy(lattice, random_empty_site, particle, neighbor_indices, epsilon_AA, epsilon_BB, epsilon_AB)
 
-        #Calculate acceptance probability for adding particle
+        # Calculate acceptance probability for adding particle
         acc_prob = min(1, (N_empty) / (N_s + 1) * np.exp(-beta * (delta_E - mu)))
 
         r = np.random.rand()
-        if r < acc_prob:    #Decide to accept the addition of the particle
-            lattice[random_empty_site] = particle
+        if r < acc_prob:    # Decide to accept the addition of the particle
+            add_x, add_y = random_empty_site
+            lattice[add_x, add_y] = particle
             if particle == 1:
-                N_A += 1    #Increment site containing particle A count
+                N_A += 1    # Increment site containing particle A count
             else:
-                N_B += 1    #Increment site containing particle B count
-            N_empty -= 1        #Decrement empty site count
+                N_B += 1    # Increment site containing particle B count
+            N_empty -= 1        # Decrement empty site count
 
 
     else:  # Removing a particle
@@ -119,12 +177,12 @@ def attempt_move(lattice, N_A, N_B, N_empty, neighbor_indices, params, count):
             return N_A, N_B, N_empty  # No particles to remove
         
 
-        #Select a random filled site
+        # Select a random filled site
         filled_site = np.argwhere(lattice != 0)     
         rand_idx = np.random.randint(len(filled_site))
         random_filled_site = filled_site[rand_idx]
-        x, y = random_filled_site
-        particle = lattice[x, y]
+        rem_x, rem_y = random_filled_site
+        particle = lattice[rem_x, rem_y]
 
 
         if particle == 1:   # Particle A
@@ -136,15 +194,15 @@ def attempt_move(lattice, N_A, N_B, N_empty, neighbor_indices, params, count):
             epsilon = epsilon_B
             N_s = N_B
 
-        #Calculate change in energy for removing particle
+        # Calculate change in energy for removing particle
         delta_E = -epsilon - calculate_interaction_energy(lattice, random_filled_site, particle, neighbor_indices, epsilon_AA, epsilon_BB, epsilon_AB)
 
-        #Calculate acceptance probability for removing particle
+        # Calculate acceptance probability for removing particle
         acc_prob = min(1, N_s / (N_empty + 1) * np.exp(-beta * (delta_E + mu)))
 
         r = np.random.rand()
-        if r < acc_prob:    #Decide to accept the removal of a particle
-            lattice[random_filled_site] = 0  # Remove particle
+        if r < acc_prob:    # Decide to accept the removal of a particle
+            lattice[rem_x, rem_y] = 0  # Remove particle
             if particle == 1:
                 N_A -= 1
             else:
@@ -157,22 +215,40 @@ def attempt_move(lattice, N_A, N_B, N_empty, neighbor_indices, params, count):
 #Run the GCMC Simulation
 def run_simulation(size, n_steps, params):
     """
+    Runs a grand canonical monte carlo simulation of competitive adsorption
+
+    Parameters:
+        size: int
+            Used to initalize a lattice of dimensions size x size
+        n_steps: int
+            Number of steps (moves) the simulation will traverse
+        params: dict{str: float}
+            Contains the values for the interaction energies, adsorption energies, and temperature
     
-    
-    
+    Returns:
+        lattice: ndarray
+            Represents the simulated lattice. Contains particle A, B or an empty site at all positions within the array
+        coverage_A: ndarray
+            Number of adsorbed particle A / total sites for each step in the simulation
+        coverage_B: ndarray
+            Number of adsorbed particle B / total sites for each step in the simulation
     """
-    lattice = initialize_lattice(size)  #Initialize lattice
-    neighbor_indices = compute_neighbor_indices(size)   #Compute neighbor indices
-    N_sites = size * size
-    N_A = 0
-    N_B = 0
-    N_empty = N_sites
-    coverage_A = np.zeros(n_steps)
-    coverage_B = np.zeros(n_steps)
+
+    lattice = initialize_lattice(size)  # Initialize lattice
+    neighbor_indices = compute_neighbor_indices(size)   # Compute neighbor indices
+    N_sites = size * size   # Compute number of empty sites
+    N_A = 0     # Initialize number of adsorped A particles
+    N_B = 0     # Initialize number of adsorped A particles
+    N_empty = N_sites   # Initialize number of empty sites
+
+    coverage_A = np.zeros(n_steps)  # Initalize  array
+    coverage_B = np.zeros(n_steps)  # Initialze array
+
+    # Compute a move in the simulation
     for step in range(n_steps):
-        N_A, N_B, N_empty = attempt_move(lattice, N_A, N_B, N_empty, neighbor_indices, params, step)
-        coverage_A[step] = N_A / N_sites
-        coverage_B[step] = N_B / N_sites
+        N_A, N_B, N_empty = attempt_move(lattice, N_A, N_B, N_empty, neighbor_indices, params)
+        coverage_A[step] = N_A / N_sites    # Update the coverage of particle A
+        coverage_B[step] = N_B / N_sites    # Update the coverage of particle B
 
     return lattice, coverage_A, coverage_B
 
@@ -180,24 +256,38 @@ def run_simulation(size, n_steps, params):
 #Plot lattice configuration
 def plot_lattice(lattice, ax, title):
     """
+    Plots particles A and B as well as empty sites on a grid representing the lattice
+
+    Parameters:
+        lattice: ndarray
+            Represents the simulated lattice. Contains particle A, B or an empty site at all positions within the array
+        ax: Axes
+            An empty subplot
+        title: str
+            Title of the subplot
     
-    
-    
+    Returns:
+        ax: Axes
+            A subplot containing adsorped A and B particles and empty sites for a given chemical potential of particle A
     """
-    size = np.shape(lattice)[0]
+
+    size = np.shape(lattice)[0]     # Get dimensions of the lattice
+    
     for x in range(size):
         for y in range(size):
-            if lattice[x, y] == 1:
-                ax.plot(x + 0.5, y+ 0.5, 'o', color='red', markersize=20)
-            elif lattice[x, y] == 2:
-                ax.plot(x + 0.5, y+ 0.5, 'o', color='blue', markersize=20)
-
-    plt.xlim(0, size)
-    plt.ylim(0, size)
-    plt.tick_params(which='both')
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(which='minor')
-    plt.title(title)
+            if lattice[x, y] == 1:  # Plot particle A
+                ax.plot(x + 0.5, y+ 0.5, 'o', color='red', markersize=10)
+            elif lattice[x, y] == 2:    # Plot particle B
+                ax.plot(x + 0.5, y+ 0.5, 'o', color='blue', markersize=10)
+    
+    # Set plot configuration
+    ax.set_xlim(0, size)
+    ax.set_ylim(0, size)
+    ax.set_xticks(np.arange(0, size + 1, 1))  
+    ax.set_yticks(np.arange(0, size + 1, 1))  
+    ax.set_xticklabels(['']*(size+1))  
+    ax.set_yticklabels(['']*(size+1))  
+    ax.grid()
+    ax.set_title(title, fontsize=8)
 
     return ax
